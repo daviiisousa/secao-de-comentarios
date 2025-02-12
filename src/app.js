76f90326028db19b0134fa467c61d3c07f6f73c6
daviiisousa <sousadavi248@gmail.com>
2025-02-11 21:08:29 -0300

    respostas

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const campo = document.getElementById("comentarios");
+const respostas = document.getElementById("respostas")
 
 async function getProduct() {
   try {
@@ -11,7 +12,7 @@ async function getProduct() {
     campo.innerHTML = data.comments
       .map(
         (comment) => `
-                <div class=divComentario>
+                <div class="divComentario">
                         <div class="perfilEScore">
                             <div class="divDoScore">
                                 <button><img src="../images/icon-plus.svg" alt="icon de mais"/></button>
@@ -42,6 +43,37 @@ async function getProduct() {
             `
       )
       .join("");
+
+      respostas.innerHTML = data.comments[1].replies.map((resposta) => `              
+                    <div class="divComentario respostas">
+                        <div class="perfilEScore">
+                            <div class="divDoScore">
+                                <button><img src="../images/icon-plus.svg" alt="icon de mais"/></button>
+                                <p class="score">${resposta.score}</p>
+                                <button>
+                                    <img src="../images/icon-minus.svg" alt="icon de menos"/>
+                                </button>
+                            
+                            </div>
+                            <div class="perfilEComentario">
+                                <div class="perfilEResponder">
+                                    <div class="divDoPerfil">
+                                        <img src=${resposta.user.image.png} />
+                                        <p class="nomeUsuario"><strong>${resposta.user.username}</strong></p>
+                                        <p class="dataDeCriaçao">${resposta.createdAt}</p>
+                                    </div>
+                                    <button class="responder">
+                                        <img src="../images/icon-reply.svg" alt="icon de compartilhamento" />
+                                        Reply
+                                    </button>
+                                </div>
+                                <article>
+                                    <p class="comentario"><span class="arrobaUsuario">@${resposta.replyingTo}</span>${resposta.content}</p>
+                                </article>
+                        </div>
+                    </div>                 
+                </div>`
+      ).join("")
   } catch (error) {
     console.error("Erro ao buscar os dados:", error);
     campo.innerHTML = "<p>Erro ao carregar os comentários.</p>";
